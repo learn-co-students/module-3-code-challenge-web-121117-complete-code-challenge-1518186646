56111cc894b0ab913d17ec1a9100786ccc510cc7
github username <github email address>
2018-02-09 11:01:07 -0500

    ALMOST DO NE

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,23 @@
 class Image {
-  
+  static init() {
+    Image.postImage();
+    let likeButton = document.getElementById("like_button");
+    likeButton.addEventListener("click", Image.addLike);
+  }
 
+  static postImage() {
+    let picture = document.getElementById("image");
+    let name = document.getElementById("name");
+    ImageAdapter.getImage(2).then(json => {
+      picture.src = json.url;
+      name.innerHTML = json.name;
+      let likes = document.getElementById("likes");
+      likes = json.like_count;
+    });
+  }
+  static addLike() {
+    let likes = document.getElementById("likes");
+    likes.innerHTML = parseInt(likes.innerHTML) + 1;
+    ImageAdapter.addLike(2, likes);
+  }
 }
